20c62b83c10e79d6c024c786148fa3d38be17b97
Lukasz <lukaszkurnik96@gmail.com>
2021-06-15 17:09:49 +0000

    pierwszy commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import  {hello}  from "./greet";
+import {aws_config} from './aws_export';
+
+import {
+	CognitoUserPool,
+	CognitoUserAttribute,
+	CognitoUser,
+} from 'amazon-cognito-identity-js';
+
+const userPool = new CognitoUserPool({
+    UserPoolId: aws_config.userPoolId,
+    ClientId: aws_config.clientId,
+});
 
 const register = (registerRequest) => {
-    console.log(registerRequest);
-}
+    userPool.signUp('193618', '123qwe', [], null, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(result);
+    })
+};
 
 const foo = 'boo';
 const registerBtn = document.querySelector('button.register');
@@ -10,7 +28,7 @@ const registerBtn = document.querySelector('button.register');
 registerBtn.addEventListener('click', () => {
     register({"name": "Eniu", "xyz": "foo"});
 });
-
+ 
 (() => {
     hello("Eniu");
 })();
